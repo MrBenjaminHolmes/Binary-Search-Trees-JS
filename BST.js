81fd8ee116a8ea7a636e881d6e6e316f81e074bc
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2024-11-03 13:01:32 +0000

    Returning Midpoint

--- BST.js
+++ BST.js
@@ -1,12 +1,10 @@
-class node {
-  constructor() {
+class Node {
+  constructor(data) {
     this.data = data;
     this.left = null;
     this.right = null;
   }
 }
-
-testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 class Tree {
   constructor(array) {
@@ -14,6 +12,24 @@ class Tree {
     this.root = this.buildTree(sortedArray);
   }
 
-  buildTree(array) {}
+  buildTree(sortedArray) {
+    console.log(sortedArray);
+    const midPoint = sortedArray[Math.floor(sortedArray.length / 2)];
+    const rootNode = new Node(midPoint);
+    return rootNode;
+  }
 }
 module.exports = Tree;
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const node = require("./BST");
 
 testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
-array = [...new Set(testArray)].sort((a, b) => a - b);
-console.log(array);
+newTree = new Tree(testArray);
+console.log(newTree.root);
